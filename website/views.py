38b3fc3caf0e85d72f3d4e746aaae90e0eb18040
lowerzedo <tp052475@mail.apu.edu.my>
2023-07-16 21:48:49 +0800

    feat: models some records and table

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +23,7 @@ def home(request):
             return redirect('home')
 
     else:
-        return render(request, 'home.html', {} )
+        return render(request, 'home.html', {'records':records} )
 
 
 
@@ -32,4 +36,19 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {} )
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have successfully Registered!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form} )
+    return render(request, 'register.html', {'form': form} )
+
